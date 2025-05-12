97f2726befbeca599197e1ef97941ef63e36226f
Kiliken <francescopaolomariani7@gmail.com>
2025-05-12 19:43:21 +0900

    EndlessRunner SmallFix

--- Games/RunGame.bat
+++
@@ -1,5 +0,0 @@
-:: Francesco Paolo Mariani
-:: なんか問題があったらchromePathを自分のパソコンのchomeパスにしてください。
-@echo off
-set chromePath="%ProgramFiles%\Google\Chrome\Application\chrome.exe"
-%chromePath% --user-data-dir="C:\VirtualBrowserUser" --allow-file-access-from-files --allow-file-access --allow-cross-origin-auth-prompt --window-size=1280,720 --hide-scrollbars --app="file:///%cd%/index.html"
